35d3d8b418a8fb6725c348563a4a8df8a17b5be9
Alexander O'Mara <me@alexomara.com>
2024-12-09 23:57:06 -0500

    Having value implies reference still good

--- set.ts
+++ set.ts
@@ -159,7 +159,7 @@ export class MeekSet<T extends WeakKey = WeakKey> {
 	 * @returns Whether the value is in this set.
 	 */
 	public has(value: T): boolean {
-		return !!this.#vwv.get(value)?.deref();
+		return !!this.#vwv.get(value);
 	}
 
 	/**
